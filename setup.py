96b1b48a9defdd279d5d0ac53b0e0e9576ea0f90
mathisarends <mathisarends27@gmail.com>
2025-04-22 22:10:38 +0200

    Update install requirements

--- setup.py
+++ setup.py
@@ -2,14 +2,15 @@ from setuptools import setup, find_packages
 
 setup(
     name="hueify",
-    version="0.1.0",
-    description="Python pacakkage to control Philips Hue lights and groups.",
+    version="0.1.2",
+    description="Python bib to control Philips Hue lights and groups.",
     author="Mathis Arends",
     url="https://github.com/mathisarends/hueify",
     packages=find_packages(),
     install_requires=[
         "aiohttp>=3.11.0",
         "python-dotenv>=1.0.0",
+        "rapidfuzz==3.13.0",
     ],
     include_package_data=True,
     python_requires=">=3.7",
